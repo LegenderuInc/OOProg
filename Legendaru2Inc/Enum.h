80681e27415bb0227fb267c8f61f89ded892c986
Shnooberella <emeilert91@gmail.com>
2014-04-06 16:37:03 +0200

    InterSone + Enum

---
+++ Legendaru2Inc/Enum.h
@@ -0,0 +1,8 @@
+#if !defined(__Enum_h)
+#define  __Enum_h
+
+enum Eiendomstype {tomt, enebolig, rekkehus, leilighet, hytte};
+enum Onske {salg, leie, begge};
+enum Boliginfo {ukentlig, snarest};
+
+#endif
--- Legendaru2Inc/Intersone.h
+++ Legendaru2Inc/Intersone.h
@@ -1,1 +1,19 @@
-asd
+#if !defined(__INTERSONE_H)
+#define __INTERSONE_H
+
+#include "Enum.h"
+#include "LISTTOOL2.H"
+
+class InterSone : Num_element {
+private:
+   int soneNr, maxPris, minAreal, minRom;
+   enum boligInfo;
+   enum interesse;
+   enum type;
+
+public:
+   
+
+};
+
+#endif
