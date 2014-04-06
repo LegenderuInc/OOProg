8441cc4027545db3af3a518e02967b8bde6bb6c1
Deadwinz <mathiasflaatt@gmail.com>
2014-04-06 15:39:07 +0200

    olol

--- Legendaru2Inc/Kunde.cpp
+++ Legendaru2Inc/Kunde.cpp
@@ -16,10 +16,11 @@ Kunde::Kunde(string fil){
 		adresse = les_text(infil);
 		poststed = lest_text(infil);
 		mail = lest_text(infil);
+		filnavn = les_text(infil);
 
 		telefon = les_tall(infil);
-		post_nr = les_tall(infil);
 		gate_nr = les_tall(infil);
+		postkode = les_tall(infil);
 		intSoner = les_tall(infil);
 
 		for( int i = 1; i <= intSoner; i++){
@@ -30,6 +31,21 @@ Kunde::Kunde(string fil){
 
 
 }
+
+void Kunde::display(){
+	cout << "\nKundens navn: " << navn
+		<<"\nKundens filnavn:" << filnavn
+		<<"\nTelefon: " << telefon
+		<<"\nAdresse: " << adresse << " " << gate_nr
+		<<"\n \t" << postkode << " " << poststed
+		<<"\nEpost adresse: " << mail;
+
+	for(int i = 1; i <= intSoner; i++){
+		intsoner[i]->display();
+	}
+
+}
+
 
 
 
