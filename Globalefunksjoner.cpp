24a30cbf24d661511d23ef871423ddecc633760e
Deadwinz <mathiasflaatt@gmail.com>
2014-04-06 06:09:43 +0200

    New func

--- Globalefunksjoner.cpp
+++ Globalefunksjoner.cpp
@@ -14,3 +14,15 @@ int les_tall(char* text, int min, int max) {
 	return tall;
 }
 
+char les_inn(char* text){
+	char * data;
+	cout << "\n" << text;
+	char temp[STRLEN];
+	cin.ignore();
+	cin.getline(temp, STRELEN);
+	data = new char(strelen(temp)+1);
+	strcpy(data, temp);
+	return data;
+}
+
+
