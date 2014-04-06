10e6d80c575c84c4ccd9af87263dc4c19cae4ce0
Shnooberella <emeilert91@gmail.com>
2014-04-06 14:59:05 +0200

    Create const.h

---
+++ Legendaru2Inc/const.h
@@ -0,0 +1,18 @@
+#if !defined(__CONST_H)
+#define __CONST_H
+
+const int MAX_TLF = 99999999;
+const int MIN_TLF = 10000000;
+const int MAX_KUNDE = 10000000000;
+const int MIN_KUNDE = 1000;
+const int MAX_SONE = 100;
+const int MIN_SONCE = 1;
+const int MAX_POST = 9999;
+const int MIN_POST = 100;
+const int MAX_OPPDRAG = 10000000000;
+const int MIN_OPPDRAG =  10000;
+const int MAX_GATE = 69;
+const int MIN_GATE = 1;
+
+
+#endif
