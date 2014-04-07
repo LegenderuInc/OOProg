3cb376a2509264c28bcd8a037d27f38867d735a7
ivaz <ivazvaka@hotmail.com>
2014-04-07 20:33:47 +0200

    glob

--- Legendaru2Inc/Globalefunksjoner.cpp
+++ Legendaru2Inc/Globalefunksjoner.cpp
@@ -3,9 +3,9 @@
 
 int les_tall(char* text, int min, int max) {
 	int tall;
-	cout << text << ": ";
+	cout << text;
 	cin >> tall;
-	while (tall <= min || tall >= max || (!cin.good())) {
+	while (tall < min || tall > max || (!cin.good())) {
 		cin.clear();
 		cin.ignore(100, '\n');
 		cout << "(" << min << " - " << max << "): ";
@@ -14,3 +14,13 @@ int les_tall(char* text, int min, int max) {
 	return tall;
 }
 
+char* les_text(char* text){
+	char * data;
+	cout << text;
+	char temp[50];
+	cin.ignore();
+	cin.getline(temp, 50);
+	data = new char[strlen(temp)+1];
+	strcpy(data, temp);
+	return data;
+}
--- Legendaru2Inc/Globalefunksjoner.h
+++ Legendaru2Inc/Globalefunksjoner.h
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <cstring>
-#include "listtool.h"
+#include <fstream>
+#include "listtool2.h"
+
 using namespace std;
 
 
 
 int les_tall(char*, int, int);			// returnerer ett heltall
-char les_inn(char* text);				// returerer text input
+char* les_text(char* text);			// returerer text input
 void lagNavn(char*t, char* s1, char* s2, int n, const int LEN);  // Lager navn i t.
+
 
 
 
