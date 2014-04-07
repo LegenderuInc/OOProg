1e90a20fd8075f589f1dd702d45ae19305d708c4
Shnooberella <emeilert91@gmail.com>
2014-04-07 22:33:50 +0200

    Update Globalefunksjoner.h

--- Legendaru2Inc/Globalefunksjoner.h
+++ Legendaru2Inc/Globalefunksjoner.h
@@ -17,6 +17,7 @@ char* les_text(char* text);			// returerer text input
 char* les_text(ifstream* inn);
 void lagNavn(char*t, char* s1, char* s2, int n, const int LEN);  // Lager navn i t.
 void les_fra_fil();
+void skrivMeny();
 
 
 
