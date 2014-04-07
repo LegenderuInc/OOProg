6ce02f6f92a462b2fcdc70c86c9ef4cc3b26d1cd
Deadwinz <mathiasflaatt@gmail.com>
2014-04-07 19:24:59 +0200

    asd

--- Legendaru2Inc/Globalefunksjoner.h
+++ Legendaru2Inc/Globalefunksjoner.h
@@ -7,7 +7,7 @@ using namespace std;
 
 int les_tall(char*, int, int);			// returnerer ett heltall
 char les_inn(char* text);				// returerer text input
-void lagNavn(char*t, char* s1, char* s2, int n, const int LEN)  // Lager navn i t.
+void lagNavn(char*t, char* s1, char* s2, int n, const int LEN);  // Lager navn i t.
 
 
 
---
+++ Legendaru2Inc/Kunder.h
@@ -0,0 +1,17 @@
+#include"Globalefunksjoner.cpp"
+#include"const.h"
+#include"Bokst.h"
+
+using namespace std;
+
+class Kunder {
+private:
+    K;
+	int forsteKunde;
+	int sistInnlagt;
+	List* Kunde;
+private:
+	Kunder();
+	~Kunder();
+	void skriv_til_fil();	
+};
